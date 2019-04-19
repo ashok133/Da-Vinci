beae2a072eca98ed18800d4f052e89376a0f13c6
Ashok <ashok05@somaiya.edu>
2019-04-19 14:12:12 -0400

    Added partial transactions update

--- public/js/all_data_fetcher.js
+++ public/js/all_data_fetcher.js
@@ -6,7 +6,10 @@ var daVinciVueRDB = new Vue ({
     allRegulations: [],
     allClients: [],
     allInvoices: [],
-    stupid: "STUPID"
+    stupid: "STUPID",
+    specificTransaction: {
+      InvoiceID: "sdasda"
+    }
   },
   methods: {
     fetchAllEmployees() {
@@ -21,11 +24,35 @@ var daVinciVueRDB = new Vue ({
 	      })
     },
     fetchAllInvoices() {
-      fetch('http://ec2-54-202-138-243.us-west-2.compute.amazonaws.com/api/invoice.php')
+      fetch('http://ec2-54-202-138-243.us-west-2.compute.amazonaws.com/api/invoice.php', {
+      })
 	      .then(response => response.json())
 	      .then (json => {
 					daVinciVueRDB.allInvoices = json;
 					console.log(daVinciVueRDB.allInvoices[0])
+				})
+	      .catch( function(err){
+	        console.log(err)
+	      })
+    },
+    fetchSpecificTransaction() {
+      console.log(document.getElementById("invoiceToFetch").value);
+      fetch('https://invoice-ledger.herokuapp.com/get_specific_invoice', {
+            method: "POST",
+            body : JSON.stringify({
+              "invoice": document.getElementById("invoiceToFetch").value
+                  // "invoice": "1211"
+            }),
+			      headers: {
+			        'Accept': 'application/json, */*',
+			        'Content-Type': 'application/json; charset=utf-8'
+			      },
+			    })
+	      .then(response => response.json())
+	      .then (json => {
+          // console.log(json);
+					daVinciVueRDB.specificTransaction = json;
+					console.log(daVinciVueRDB.specificTransaction)
 				})
 	      .catch( function(err){
 	        console.log(err)
@@ -63,6 +90,47 @@ var daVinciVueRDB = new Vue ({
 	      .catch( function(err){
 	        console.log(err)
 	      })
+    },
+    addTransaction() {
+      fetch('https://invoice-ledger.herokuapp.com/add_invoice', {
+			      // body : JSON.stringify({
+            //
+			      //     "clientId": document.getElementById("clientId").value,
+			      //     "notes": document.getElementById("comment").value
+            //
+			      // }),
+            body : JSON.stringify( {
+              "InvoiceID": document.getElementById("InvoiceID").value,
+              "RegulationID": document.getElementById("RegulationID").value,
+              "ContractID": document.getElementById("ContractID").value,
+              "EmployeeID": document.getElementById("EmployeeID").value,
+              "ClientID": document.getElementById("ClientID").value,
+              "DateAdded": document.getElementById("DateAdded").value,
+              "Stage": document.getElementById("Stage").value,
+              "BillToContactName": document.getElementById("BillToContactName").value,
+              "BillToAddress": document.getElementById("BillToAddress").value,
+              "BillToPhone": document.getElementById("BillToPhone").value,
+              "BillToEmail": document.getElementById("BillToEmail").value,
+              "BilledProductID": document.getElementById("BilledProductID").value,
+              "BilledProductDesc": document.getElementById("BilledProductDesc").value,
+              "BilledProductCost": document.getElementById("BilledProductCost").value,
+              "BilledProductQuantity": document.getElementById("BilledProductQuantity").value,
+              "BilledProductLineTotal": document.getElementById("BilledProductLineTotal").value,
+              "ConcessionBool": document.getElementById("ConcessionBool").value,
+              "ConcessionValue": document.getElementById("ConcessionValue").value,
+              "Total": document.getElementById("Total").value,
+              "AmountDue": document.getElementById("AmountDue").value,
+              "AmountPaid": document.getElementById("AmountPaid").value,
+              "PaymentDueDate": document.getElementById("PaymentDueDate").value,
+              "Notes": document.getElementById("Notes").value,
+            }),
+			      // mode: "no-cors", // no-cors, cors, *same-origin
+			      headers: {
+			        'Accept': 'application/json, text/plain, */*',
+			        'Content-Type': 'application/json; charset=utf-8'
+			      },
+			      method: "POST"
+			    })
     }
   },
   created() {
